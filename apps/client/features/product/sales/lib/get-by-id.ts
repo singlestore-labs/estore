38e6abb47051c234fc50c857caa06f33fb3af26e
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2024-04-23 12:02:38 -0700

    getProductSales implemented

--- apps/client/features/product/sales/lib/get-by-id.ts
+++ apps/client/features/product/sales/lib/get-by-id.ts
@@ -1,28 +1,38 @@
 import { db } from "@repo/db";
-import { ORDERS_TABLE_NAME } from "@repo/db/constants";
-import { OrderRow } from "@repo/db/types";
+import { ORDERS_TABLE_NAME, PRODUCTS_TABLE_NAME } from "@repo/db/constants";
 
 import { Product } from "@/product/types";
 
 export async function getProductSales(
   filter: Pick<Product, "id"> | Pick<Product, "description">,
+  length: number = 30,
 ): Promise<Product["sales"]> {
   try {
+    const [[filterKey, filterValue]] = Object.entries(filter);
+
+    const product = await db.controllers.findOne({
+      collection: PRODUCTS_TABLE_NAME,
+      where: `LOWER(${filterKey}) = ${typeof filterValue === "string" ? filterValue.toLowerCase() : filterValue}`,
+    });
+
+    if (!product) return [];
+
     let query = `\
-      SELECT * FROM ${ORDERS_TABLE_NAME}
-      WHERE createdAt >= CURDATE() - INTERVAL 90 DAY
+      SELECT COUNT(*) AS value, DATE(createdAt) AS date FROM ${ORDERS_TABLE_NAME}
+      WHERE createdAt >= CURDATE() - INTERVAL ${length} DAY
+      AND productId = ${product.id}
+      GROUP BY DATE(createdAt)
+      ORDER BY DATE(createdAt)
     `;
 
-    if ("id" in filter) {
-      query += ` AND id = ${filter.id}`;
-    } else if ("description" in filter) {
-      query += ` AND description = ${filter.description.toLowerCase()}`;
-    }
-
-    const rows = await db.controllers.query<OrderRow[]>({ query });
-
-    return [];
+    return (await db.controllers.query<Product["sales"]>({ query }))
+      .map((i) => ({
+        ...i,
+        date: new Date(i.date).toLocaleDateString("en-US", { hour12: false }),
+      }))
+      .slice(0, length);
   } catch (error) {
+    console.log(error);
     return [];
   }
 }
