86601cc3ff334a7e6156e682f892f6dd659e066e
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2024-04-23 14:21:41 -0700

    Chat shortcuts updated

--- apps/client/data/chat-shortcuts.ts
+++ apps/client/data/chat-shortcuts.ts
@@ -2,15 +2,19 @@ import { ChatShortcut } from "@/chat/shortcut/types";
 
 export const chatShortcuts: ChatShortcut[] = [
   {
-    title: "Recommend five products",
-    description: "Provides product recommendations based on the user preferences.",
+    title: "Five random products",
+    description: "Displays five random products.",
   },
   {
-    title: "Show the top product",
+    title: "Five recommended products",
+    description: "Displays five reccomended products based on your preferences.",
+  },
+  {
+    title: "Random product sales",
+    description: "Displays a chart of a product's sales history.",
+  },
+  {
+    title: "The top product",
     description: "Displays the top product, based on sales data and likes.",
-  },
-  {
-    title: "Show random product sales",
-    description: "Displays a chart of the sales history for the top-selling product.",
   },
 ];
