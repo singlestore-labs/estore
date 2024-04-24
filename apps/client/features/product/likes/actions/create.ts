5a5966b874d56e10c0ba6eac43ac0e09ac0b83aa
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2024-04-24 10:32:12 -0700

    revalidatePath on like added

--- apps/client/features/product/likes/actions/create.ts
+++ apps/client/features/product/likes/actions/create.ts
@@ -3,8 +3,10 @@
 import { db } from "@repo/db";
 import { PRODUCT_LIKES_TABLE_NAME } from "@repo/db/constants";
 import { normalizeDate } from "@repo/helpers";
+import { revalidatePath } from "next/cache";
 
 import { forwardActionError } from "@/action/error/lib/forward";
+import { ROUTES } from "@/constants/routes";
 import { ProdcutLike } from "@/product/likes/types";
 import { Product } from "@/product/types";
 import { getUserId } from "@/user/lib/get-id";
@@ -14,10 +16,12 @@ export async function createProductLike(productId: Product["id"]) {
     const userId = await getUserId();
     if (!userId) throw new Error("userId is undefined");
     const like: ProdcutLike = { createdAt: normalizeDate(new Date()), productId, userId };
-    return (await db.controllers.insertOne({
+    const newLike = (await db.controllers.insertOne({
       collection: PRODUCT_LIKES_TABLE_NAME,
       value: like,
     })) as ProdcutLike;
+    revalidatePath(ROUTES.ROOT);
+    return newLike;
   } catch (error) {
     return forwardActionError(error);
   }
