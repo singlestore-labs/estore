fc942d628f9e7d9d4809243879a49fc1ec5d6d79
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2024-04-30 11:45:33 -0700

    dataset ids fixed

--- apps/data/lib/generate.ts
+++ apps/data/lib/generate.ts
@@ -30,7 +30,7 @@ const normalizedDatasetPath = path.join(process.cwd(), "source/normalized-datase
   );
   const created_at = normalizeDate(new Date());
 
-  const userRows: UserRow[] = Array.from({ length: USERS_NUMBER }).map((_, id) => ({ id, created_at }));
+  const userRows: UserRow[] = Array.from({ length: USERS_NUMBER }).map((_, i) => ({ id: i + 1, created_at }));
   await writeToJSON(USERS_TABLE_NAME, userRows);
 
   let productRows: ProductRow[] = [];
@@ -38,7 +38,7 @@ const normalizedDatasetPath = path.join(process.cwd(), "source/normalized-datase
   if (existsSync(productsPath)) {
     productRows = JSON.parse(await readFile(productsPath, "utf-8"));
   } else {
-    let productId = 0;
+    let productId = 1;
     for await (const chunk of toChunks(dataset, 1000)) {
       const descriptionVs = await db.ai.createEmbedding(chunk.map((i) => i.description));
       const images = await Promise.all(
@@ -68,17 +68,17 @@ const normalizedDatasetPath = path.join(process.cwd(), "source/normalized-datase
   let productSizeRows: ProductSizeRow[];
   const sizes = new Set<string>();
   productRows.forEach(({ id }) => {
-    Object.keys(dataset[id].sizesInStock).forEach((i) => sizes.add(i));
+    Object.keys(dataset[id - 1].sizesInStock).forEach((i) => sizes.add(i));
   });
-  productSizeRows = [...sizes].map((value, id) => ({ id, value }));
+  productSizeRows = [...sizes].map((value, i) => ({ id: i + 1, value }));
   await writeToJSON(PRODUCT_SIZES_TABLE_NAME, productSizeRows);
 
-  let productSKUId = 0;
-  let prodcutSKURows: ProductSKURow[] = dataset.flatMap((product, product_id) => {
-    return Object.entries(product.sizesInStock).map(([size, stock]) => {
+  let productSKUId = 1;
+  let prodcutSKURows: ProductSKURow[] = productRows.flatMap((product) => {
+    return Object.entries(dataset[product.id - 1].sizesInStock).map(([size, stock]) => {
       return {
         id: productSKUId++,
-        product_id,
+        product_id: product.id,
         product_size_id: productSizeRows.find((i) => i.value === size)!.id,
         stock,
       } satisfies ProductSKURow;
@@ -86,16 +86,16 @@ const normalizedDatasetPath = path.join(process.cwd(), "source/normalized-datase
   });
   await writeToJSON(PRODUCT_SKU_TABLE_NAME, prodcutSKURows);
 
-  const productLikeRows: ProductLikeRow[] = Array.from({ length: PRODUCT_LIKES_NUMBER }).map((_, id) => ({
-    id,
+  const productLikeRows: ProductLikeRow[] = Array.from({ length: PRODUCT_LIKES_NUMBER }).map((_, i) => ({
+    id: i + 1,
     created_at,
     user_id: getRandomArrayItem(userRows).id,
     product_id: getRandomArrayItem(productRows).id,
   }));
   await writeToJSON(PRODUCT_LIKES_TABLE_NAME, productLikeRows);
 
-  const orderRows: OrderRow[] = Array.from({ length: UNIQUE_ORDERS_NUMBER }).map((_, id) => ({
-    id,
+  const orderRows: OrderRow[] = Array.from({ length: UNIQUE_ORDERS_NUMBER }).map((_, i) => ({
+    id: i + 1,
     created_at: normalizeDate(getRandomDate(new Date(2024, 0, 1))),
     user_id: getRandomArrayItem(userRows).id,
     product_sku_id: getRandomArrayItem(prodcutSKURows).id,
