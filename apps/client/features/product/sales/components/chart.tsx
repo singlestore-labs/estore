e569093a2915cffcd7b2b00d80ba4601a03e9a8c
Yaroslav Demenskyi <yaroslav.demenskiy@gmail.com>
2024-05-07 11:53:03 -0700

    ProductDialog sales chart animation disabled

--- apps/client/features/product/sales/components/chart.tsx
+++ apps/client/features/product/sales/components/chart.tsx
@@ -1,18 +1,24 @@
 import { ComponentProps } from "@/types";
-import { ChartArea } from "@/components/chart/area";
+import { ChartArea, ChartAreaProps } from "@/components/chart/area";
 import { Card, CardProps } from "@/components/ui/card";
 import { Product } from "@/product/types";
 import { cn } from "@/ui/lib";
 
-export type ProductSalesChartProps = ComponentProps<CardProps, { sales: Product["sales"] }>;
+export type ProductSalesChartProps = ComponentProps<
+  CardProps,
+  { sales: Product["sales"]; areaProps?: ChartAreaProps["areaProps"] }
+>;
 
-export function ProductSalesChart({ className, sales, ...props }: ProductSalesChartProps) {
+export function ProductSalesChart({ className, sales, areaProps, ...props }: ProductSalesChartProps) {
   return (
     <Card
       {...props}
       className={cn("relative h-48", className)}
     >
-      <ChartArea data={sales} />
+      <ChartArea
+        data={sales}
+        areaProps={areaProps}
+      />
     </Card>
   );
 }
